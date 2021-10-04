2115e4554b68fc986bcf8c35beba209f6d7787e4
Zephtos <54938618+Zephtos@users.noreply.github.com>
2021-10-04 13:26:45 +0200

    Implemented routing
    And made simple bootstrap frontend

--- open-bee/src/app/app.module.ts
+++ open-bee/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MarketsComponent } from './markets/markets.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarketsComponent
+    MarketsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
